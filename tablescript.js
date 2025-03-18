28678c9b959eb8e060fac1478cd270c8aed76cbf
chiboyL <chinonsoekezie3@gmail.com>
2025-03-17 19:33:16 -0900

    feat: created a new file to store users data entered in the index page

---
+++ tablescript.js
@@ -0,0 +1,4 @@
+let tabledata = document.getElementById('tabledata');
+let form = document.getElementById('form');
+let formdata = JSON.parse(localStorage.getItem('userData'));
+console.log(formdata);
